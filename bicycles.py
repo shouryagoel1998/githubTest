e1cd756aa0aa7137bb2f9a0cb7cc8707e8ce2087
shouryagoel1998 <42922804+shouryagoel1998@users.noreply.github.com>
2021-01-31 12:45:14 +0530

    Add files via upload

---
+++ bicycles.py
@@ -0,0 +1,15 @@
+bicycles = ['trek','cannondale','redline','specilized']
+#                0         1           2           3
+#               -4        -3          -2          -1
+#print(bicycles)
+print(bicycles[0])      
+print(bicycles[1])    
+print(bicycles[2])      
+print(bicycles[3]) 
+print(bicycles[-1])
+print(bicycles[-2])
+print(bicycles[-3])
+print(bicycles[-4])
+message = "My first bicycles was a "+ bicycles[0].title() + "."
+print(message)
+
